ad41086e68fc88ed6f8a2d8453508381f3563fc7
Reymon <rvega@jaumebalmes.net>
2023-01-12 20:55:25 +0100

    Version 1 - Practica 10

---
+++ muertesTotales.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+# 1 funció que mostri, les morts totals per mes per ciutat (agafar 10 ciutats)
+
+dif
---
+++ numeroDeMuertos.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# 1 funció que mostri el número de morts per mes per país (agafar 10 països
+
+def numeroDeMuertos():
+
+
